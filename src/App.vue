cfaa7c278b1d1d8d0c25c215e95b47e6ea9bed54
@Wu55911584 <2962545278@qq.com>
2023-03-16 13:24:42 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>第一次</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+html,body,#app {
+position: relative;
+width: 100%;
+height: 100%;
+margin: 0;
+}
+</style>
